e678c23c5b9735409fe1b7713e674756b772342f
Sheheryar Ahmad <sheheryarahmad5e123@gmail.com>
2026-05-23 15:33:49 +0400

    remove root test files

--- test.py
+++
@@ -1,9 +0,0 @@
-from memex import Memory
-
-mem = Memory()
-
-mem.save("User prefers Python")
-
-print(mem.recall("favorite language"))
-print(mem.recall("what does she prefer"))
-# User prefers Python
